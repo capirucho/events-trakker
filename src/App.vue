3c853cb7775074c33d3eed0c126c23ca72d0885c
rfigueroa <friedbeings@gmail.com>
2019-05-28 15:35:16 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link :to="{ name: 'event-list' }">List</router-link>&nbsp;|&nbsp;
+      <router-link :to="{ name: 'event-show' }">Show Event 1</router-link>|&nbsp;
+      <router-link :to="{ name: 'event-create' }">Create Event</router-link>|&nbsp;
+      <router-link :to="{ name: 'user', params: { username: 'Ronald' } }">Ronald</router-link>
     </div>
     <router-view />
   </div>
@@ -10,7 +12,7 @@
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
